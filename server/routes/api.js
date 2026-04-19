ab69229c8027029382038ea986bca8b199ecb929
Michael Robinson <mhr2964@rit.edu>
2026-04-18 22:29:13 -0400

    Add /api/status health check endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,4 +5,8 @@ router.get('/hello', (req, res) => {
   res.json({ message: 'Hello from the API' });
 });
 
+router.get('/status', (req, res) => {
+  res.json({ status: 'ok', app: 'KnowTheW' });
+});
+
 module.exports = router;
